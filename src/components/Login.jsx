3d6afc865905d8a2cc4c4399cfc99e34426cf43c
Nikhil26062000 <nikhilsbn06@gmail.com>
2023-10-17 19:05:47 +0530

    Made responsive UI

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,13 +10,13 @@ const Login = () => {
         setIsSign(!isSign);
     }
   return (
-    <div>
+    <div className='loginPage'>
         <Header />
-        <div className='absolute'>
+        <div className='backgroundImage absolute'>
             <img src="https://assets.nflxext.com/ffe/siteui/vlv3/ab180a27-b661-44d7-a6d9-940cb32f2f4a/7fb62e44-31fd-4e1f-b6ad-0b5c8c2a20ef/IN-en-20231009-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt='backgroundImage' />
         </div>
 
-        <form className='p-12 bg-black absolute w-3/12 my-32 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+        <form className='formStyle p-12 bg-black absolute w-[400px] my-24 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
 
             <h1 className='font-bold text-3xl py-4'>{isSign ? "Sign In":"Sign Up"}</h1>
 
