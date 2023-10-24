b67ad9e27ae1fc608c16a7b1679340f7638d9953
Nikhil26062000 <nikhilsbn06@gmail.com>
2023-10-24 16:37:05 +0530

    Redesign the logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="Header absolute z-10 ">
-        <img className='w-44 py-2 px-4 bg-gradient-to-b from-black' src="https://logodix.com/logo/272156.jpg" alt="logo"/>
+        <img className='w-44 py-2 px-4' src="https://static.vecteezy.com/system/resources/previews/003/374/886/non_2x/white-red-letter-n-alphabet-logo-design-icon-for-company-vector.jpg" alt="logo"/>
     </div>
   )
 }
