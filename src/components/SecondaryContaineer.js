17a69add23261098147c8760517a108858850cfc
Nikhil26062000 <nikhilsbn06@gmail.com>
2023-11-02 20:36:28 +0530

    initial commit in development branch

--- src/components/SecondaryContaineer.js
+++ src/components/SecondaryContaineer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SecondaryContaineer = () => {
   return (
-    <div>SecondaryContaineer</div>
+    <div>This is Secondary containeer under development branch</div>
   )
 }
 
